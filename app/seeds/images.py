12716a3d6401705a2dfa7541941f281767b1fab9
AndrewPMurray <andrewpmurray1987@gmail.com>
2022-03-06 18:31:07 -0500

    cat seeders complete

--- app/seeds/images.py
+++ app/seeds/images.py
@@ -46,6 +46,106 @@ def seed_images():
         product_id=3,
         url="https://i.etsystatic.com/5376178/r/il/e0193c/2454259064/il_794xN.2454259064_c48u.jpg"
     )
+    cat_sunglasses1 = Image(
+        product_id=4,
+        url="https://i.etsystatic.com/27752622/r/il/e0202b/3238848903/il_794xN.3238848903_5odu.jpg"
+    )
+    cat_sunglasses2 = Image(
+        product_id=4,
+        url="https://i.etsystatic.com/27752622/r/il/a45b35/3056695774/il_794xN.3056695774_cy00.jpg"
+    )
+    cat_sunglasses3 = Image(
+        product_id=4,
+        url="https://i.etsystatic.com/27752622/r/il/09dfce/3104439135/il_794xN.3104439135_22nc.jpg"
+    )
+    cat_cave1 = Image(
+        product_id=5,
+        url="https://i.etsystatic.com/32942464/r/il/214a67/3628927845/il_794xN.3628927845_c7hr.jpg"
+    )
+    cat_cave2 = Image(
+        product_id=5,
+        url="https://i.etsystatic.com/32942464/r/il/60d6b3/3520385980/il_794xN.3520385980_33t1.jpg"
+    )
+    cat_cave3 = Image(
+        product_id=5,
+        url="https://i.etsystatic.com/32942464/r/il/15adb2/3552416170/il_794xN.3552416170_ajbd.jpg"
+    )
+    cat_tree1 = Image(
+        product_id=6,
+        url="https://i.etsystatic.com/32103991/r/il/804de3/3532964418/il_794xN.3532964418_7u07.jpg"
+    )
+    cat_tree2 = Image(
+        product_id=6,
+        url="https://i.etsystatic.com/32103991/r/il/80ae8f/3532964384/il_794xN.3532964384_dh9z.jpg"
+    )
+    cat_tree3 = Image(
+        product_id=6,
+        url="https://i.etsystatic.com/32103991/r/il/046d82/3532964414/il_794xN.3532964414_mno6.jpg"
+    )
+    cat_tree4 = Image(
+        product_id=6,
+        url="https://i.etsystatic.com/32103991/r/il/208cfe/3580606851/il_794xN.3580606851_g54q.jpg"
+    )
+    cat_tree5 = Image(
+        product_id=6,
+        url="https://i.etsystatic.com/32103991/r/il/dd03f3/3532964410/il_794xN.3532964410_ruvs.jpg"
+    )
+    cat_food_lid1 = Image(
+        product_id=7,
+        url="https://i.etsystatic.com/26339049/r/il/2d5743/3039223065/il_794xN.3039223065_qmbj.jpg"
+    )
+    cat_food_lid2 = Image(
+        product_id=7,
+        url="https://i.etsystatic.com/26339049/r/il/568212/3462732773/il_794xN.3462732773_6l7e.jpg"
+    )
+    cat_food_lid3 = Image(
+        product_id=7,
+        url="https://i.etsystatic.com/26339049/r/il/8a9620/3415061478/il_794xN.3415061478_6the.jpg"
+    )
+    cat_food_bowl1 = Image(
+        product_id=8,
+        url="https://i.etsystatic.com/17860591/r/il/0cc628/2436749262/il_794xN.2436749262_4b8a.jpg"
+    )
+    cat_food_bowl2 = Image(
+        product_id=8,
+        url="https://i.etsystatic.com/17860591/r/il/b62213/1756938766/il_794xN.1756938766_4d1p.jpg"
+    )
+    cat_food_bowl3 = Image(
+        product_id=8,
+        url="https://i.etsystatic.com/17860591/r/il/c99590/2550231124/il_794xN.2550231124_9250.jpg"
+    )
+    cat_bird_toy1 = Image(
+        product_id=9,
+        url="https://i.etsystatic.com/22528987/r/il/63cba3/3369244293/il_794xN.3369244293_k358.jpg"
+    )
+    cat_bird_toy2 = Image(
+        product_id=9,
+        url="https://i.etsystatic.com/22528987/r/il/bbdc6d/3369316675/il_794xN.3369316675_35x5.jpg"
+    )
+    cat_bird_toy3 = Image(
+        product_id=9,
+        url="https://i.etsystatic.com/22528987/r/il/1bd341/3321553658/il_794xN.3321553658_ivik.jpg"
+    )
+    cat_cardigan1 = Image(
+        product_id=10,
+        url="https://i.etsystatic.com/24610013/r/il/b7129b/3295270098/il_794xN.3295270098_ikxy.jpg"
+    )
+    cat_cardigan2 = Image(
+        product_id=10,
+        url="https://i.etsystatic.com/24610013/r/il/9f27df/3342963105/il_794xN.3342963105_7u1r.jpg"
+    )
+    cat_cardigan3 = Image(
+        product_id=10,
+        url="https://i.etsystatic.com/24610013/r/il/8e9727/3295270092/il_794xN.3295270092_em7d.jpg"
+    )
+    cat_cardigan4 = Image(
+        product_id=10,
+        url="https://i.etsystatic.com/24610013/r/il/7f3c9f/3295278410/il_794xN.3295278410_t6m1.jpg"
+    )
+    cat_cardigan5 = Image(
+        product_id=10,
+        url="https://i.etsystatic.com/24610013/r/il/3ba614/3295270888/il_794xN.3295270888_jesv.jpg"
+    )
     
     db.session.add(cat_slipper1)
     db.session.add(cat_slipper2)
@@ -58,6 +158,30 @@ def seed_images():
     db.session.add(cat_gift_box3)
     db.session.add(cat_gift_box4)
     db.session.add(cat_gift_box5)
+    db.session.add(cat_sunglasses1)
+    db.session.add(cat_sunglasses2)
+    db.session.add(cat_sunglasses3)
+    db.session.add(cat_cave1)
+    db.session.add(cat_cave2)
+    db.session.add(cat_cave3)
+    db.session.add(cat_tree1)
+    db.session.add(cat_tree2)
+    db.session.add(cat_tree3)
+    db.session.add(cat_tree4)
+    db.session.add(cat_tree5)
+    db.session.add(cat_food_lid1)
+    db.session.add(cat_food_lid2)
+    db.session.add(cat_food_lid3)
+    db.session.add(cat_food_bowl1)
+    db.session.add(cat_food_bowl2)
+    db.session.add(cat_bird_toy1)
+    db.session.add(cat_bird_toy2)
+    db.session.add(cat_bird_toy3)
+    db.session.add(cat_cardigan1)
+    db.session.add(cat_cardigan2)
+    db.session.add(cat_cardigan3)
+    db.session.add(cat_cardigan4)
+    db.session.add(cat_cardigan5)
     
     db.session.commit()
 
--- app/seeds/products.py
+++ app/seeds/products.py
@@ -22,7 +22,7 @@ Dimensions (approximate):
         user_id=4,
         product_type_id=4,
         pet_type_id=1
-        )
+    )
     
     cat_weed = Product(
         title='Catbuzz Premium and Organically Grown Catnip | Fresh | Grown by Family Farmers in USA | All-Natural | Eco-Friendly',
@@ -106,12 +106,187 @@ darn!socks: I’m just an old holey sock, filled with organic catnip, recycled c
         user_id=5,
         product_type_id=2,
         pet_type_id=1
-        )
+    )
+
+    cat_sunglasses = Product(
+        title='Sunglasses for Cats (11 Color+ Bonus) | Cool Funny Novelty Cat Glasses ',
+        price=7.99,
+        details='["Handmade","Materials: cat sunglasses, glasses for cat, sunglasses for cats, cat glasses, cat funny"]',
+        description="""
+#1 Cat Sunglasses on the Market
+
+~~11 Color Options~~
+
+BONUS: Comes with adjustable strap.
+
+Turn your Cat into a cute & funny furry pet model.
+
+Latest fashion clothing -cat glasses.
+
+FAST SHIPPING 
+        """,
+        quantity=22,
+        user_id=4,
+        product_type_id=4,
+        pet_type_id=1
+    )
+    
+    cat_cave = Product(
+        title='Organic Wool Cat Bed Cave (Large/Medium) - Eco Friendly 100% Merino Wool Beds for Cats and Kittens ',
+        price=49.99,
+        details='["Handmade"]',
+        description="""
+Chiby Cat Cave
+
+Our team of pet enthusiasts created beautiful designs for your home and your furry friends. Handcrafted from 100% Natural Wool. It is very sturdy. Classy yet modern minimalist design that almost looks like furniture. Flip through our designs to find the right one to go perfectly with your decor. You can find different colors of Cat Cave in our collection.
+
+Chiby Cat Caves are made by ancient hand felting technique, designed to last forever. Most importantly, it is an eco-friendly and sustainable choice. Select your prefered cat cave from our variety of collection. Let’s support small businesses that help sustain and provide help to the lovely Nepalese craftsman. Each piece is uniquely crafted by hand hence, no two caves are alike. There might be slight differences in size, color and weight.
+
+Reviews, comments and suggestions are highly appreciated. Also, please feel free to share photos and or videos of your furry friend enjoying their custom cat caves.
+
+Thank you for choosing us.
+Namaste!
+
+A spacious room
+Specifically designed for inhouse cats and kittens of all sizes. Spacious enough to move around. Cats and Kittens all over the world have found comfort and home in these tiny beds.
+
+Sustainable purchase
+It is 100% biodegradable, renewable, sustainable and certified cruelty-free kiwi wool. Each cat cave is hand-felted with love from 100% natural organic wool to ensure a happy and healthy kitty.
+
+Cat friendly
+Our handmade wool wards off dirt, smell and stain marks. The natural scent of the felt is an easy attraction for cats. Your kitty will love to curl up & play in it. Your cat can either hide in it or lay on it. The felt cave will pop right back to its normal shape.
+
+Easily Washable
+You can lightly vacuum it or hand wash it with lukewarm water & detergent. Gently press out the moisture and shape it back to its natural dome size.
+
+Convenient Product
+A personal lair for your fluffy friend to hang out in. These individually made cat caves absorb moisture and insulate against heat. It offers natural properties to keep your pets cleaner and 
+        """,
+        quantity=5,
+        user_id=5,
+        product_type_id=3,
+        pet_type_id=1
+    )
+
+    cat_tree = Product(
+        title='3 Tier Soft Plush Cat Tree with Condo | Cat Tree Tower with Double Condo and Top Perch | Scratching Poles, Scratch Ramp & leisure sisal rope ',
+        price=95.99,
+        details='["Handmade"]',
+        description="""
+1.STURDY AND SAFE: Stability is always priority. The bottom base is strengthened that ensure its stability of the whole cat tree.
+
+2.WIDEN AND SPACIOUS: The extra-large top perch with raised rim offer maximum comfort for feline friend while they are lounging and looking out window, they will definitely enjoy it!
+
+3.COZY AND PRIVACY: The dual condos are covered with ultra-soft plush and the enclosed design creating a sense of security provide them a quiet place to curl up for snoozing.
+
+4.HEALTY AND FUN: Full wrapped with natural sisal, the scratching post will completely satisfy their instinct of scratching and help them maintaining a healthy habits of clawing!
+
+5.REASONABLE AND DETACHABLE: Your lovely cats are fascinated with the fuzzy ball, which is detachable. 
+        """,
+        quantity=1,
+        user_id=5,
+        product_type_id=3,
+        pet_type_id=1
+    )
+    
+    cat_food_lids = Product(
+        title='Custom Cat Food Lids, 3D Printed ',
+        price=5.00,
+        details='["Handmade","Materials: PLA, 3D Print, 3D Printed, 3D Printing"]',
+        description="""
+What do you do when you cat doesn’t finish all of their food? Or decides that they would like a little of several different flavors! Use lids for your cat’s wet food! Click to shop now or request a custom design!
+
+These rigid lids are designed to fit most 3oz cans of cat food, including Fancy Feast, Purina, Merrick, Blue, and Hill's Diet. (If you use a brand not listed, please reach out and we will check to make sure that our lids fit your brand!) They snap on and stay put. No more open cans in the fridge and no more wasting food! It’s a win-win.
+
+You may choose to have a name, initials or no text on the lid.
+
+If you are needing a different size or brand, contact us and we will work with you!
+
+This product is made from PLA (eco-friendly!) with a 3D printer. Hand wash only.
+
+Please allow 3-5 business days for custom printing. Returns are not allowed on customized items.
+
+If you have something custom in mind, please feel free to contact us!
+
+DISCLAIMER: All objects are custom made to order. The 3D printing process means that there may be slight imperfections in any 3D printed object, but everything will be similar to the pictures shown. Due to the 3D printing process and variations in monitor settings, the color of your finished item may vary in color.
+        """,
+        quantity=20,
+        user_id=4,
+        product_type_id=1,
+        pet_type_id=1
+    )
+    
+    cat_food_bowl = Product(
+        title='Funny Cat Food Bowl, Ceramic Pet Bowl ',
+        price=28.00,
+        details='["Handmade","Materials: ceramic, ink"]',
+        description="""
+DESCRIPTION:
+Ceramic pet bowl. Perfect gift for any pet owner.
+Bowl is DISHWASHER and MICROWAVE SAFE.
+Made with dye sublimation printing, which prints the design into the product instead of on it, ensuring a permanent image.
+Bowl is white.
+Image is on one side.
+
+Available in 2 Sizes:
+Small- Approx. 6 inches wide by 2.25 inches tall, holds a maximum of 3 cups.
+Large- Approx. 7 inches wide by 2.85 inches tall, holds a maximum of 5 cups.
+        """,
+        quantity=11,
+        user_id=5,
+        product_type_id=1,
+        pet_type_id=1
+    )
+        
+    cat_bird_toy = Product(
+        title='Cat Toys, Cat Treats, Cat Toy, Catnip Toy, Cat Lover, Bird ',
+        price=9.99,
+        details='["Handmade"]',
+        description="""
+This bird is designed to stimulate your cat. The toy is 5.7 x 2.76 x 2.17 inches in size and suitable for all cats. The bird only squeaks when touched. Remove the plastic cord to activate the bird. The bird is made from soft real feathers and fleece. 
+        """,
+        quantity=8,
+        user_id=4,
+        product_type_id=2,
+        pet_type_id=1
+    )
+
+    cat_cardigan = Product(
+        title='Customize Sphynx Hairless cat, Devon Rex Contrast color popular autumn and winter cardigan pet sweater for doggie and cat as well ',
+        price=14.96,
+        details='["Handmade"]',
+        description="""
+***The underwear T­‒Shirt and the necklace need to be purchased separately. ***
+
+***If you place the order Sweater+Underwear, we will send the one pair of glasses for free. ***
+
+Original design and hand knitted. All Materials are safe to baby, so safe to our baby cats.
+
+Underage cats grow faster, so it is recommended to choose one bigger size.
+
+For example, a cat over 4 months weighs 2.2 kg. Due to its growth so fast, it is recommended to choose size M.
+
+For our experience and plus the shipping time, we suggest the cat under 4 months, choose M, the adult cat choose either L or XL.
+
+As we all know that the skin of hairless cat is like a baby and needs protection well. All fabrics in our studio are selected strictly and all clothes are hand-made and can be customized. Cats will not have any allergic reactions when wearing our products. There are many cats in our studio who are our best inspectors. The clothes are washable. The pure cotton fabric may pilling after being worn for a period of time. That's because the cat's tongue is prickly and always loves to lick. It’s not a matter of clothes or material. Please kindly be aware of it.
+        """,
+        quantity=10,
+        user_id=5,
+        product_type_id=4,
+        pet_type_id=1
+    )
 
 
     db.session.add(cat_slippers)
     db.session.add(cat_weed)
     db.session.add(cat_gift_box)
+    db.session.add(cat_sunglasses)
+    db.session.add(cat_cave)
+    db.session.add(cat_tree)
+    db.session.add(cat_food_lids)
+    db.session.add(cat_food_bowl)
+    db.session.add(cat_bird_toy)
+    db.session.add(cat_cardigan)
 
     db.session.commit()
 
