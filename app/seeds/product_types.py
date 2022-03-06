d67c4c02e6df99f2d6f82989fd18d01a02610c3b
AndrewPMurray <andrewpmurray1987@gmail.com>
2022-03-06 11:10:09 -0500

    added 3 cat products to seeders

--- app/seeds/product_types.py
+++ app/seeds/product_types.py
@@ -1,7 +1,6 @@
 from app.models import db, ProductType
 
 
-# Adds a demo user, you can add other users here if you want
 def seed_product_types():
     food = ProductType(title='Food', url='url')
     toys = ProductType(title='Toys', url='url')
@@ -17,11 +16,6 @@ def seed_product_types():
     db.session.commit()
 
 
-# Uses a raw SQL query to TRUNCATE the users table.
-# SQLAlchemy doesn't have a built in function to do this
-# TRUNCATE Removes all the data from the table, and RESET IDENTITY
-# resets the auto incrementing primary key, CASCADE deletes any
-# dependent entities
 def undo_product_types():
     db.session.execute('TRUNCATE product_types RESTART IDENTITY CASCADE;')
     db.session.commit()
