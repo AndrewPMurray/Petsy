11966ae9022d291e7c7f88105ad6dc053d843521
AndrewPMurray <andrewpmurray1987@gmail.com>
2022-03-07 18:07:29 -0500

    listing form started

--- react-app/src/components/ListingForm/index.js
+++ react-app/src/components/ListingForm/index.js
@@ -1,5 +1,8 @@
 import './ListingForm.css';
+import { useState } from 'react';
 
-export default function ListingForm() {
+export default function ListingForm({ product }) {
+	const [title, setTitle] = useState(product?.title || '');
+
 	return <h2>listing form!</h2>;
 }
