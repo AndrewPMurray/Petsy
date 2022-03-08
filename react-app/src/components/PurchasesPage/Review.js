183c35c8f44739d369da4a56f60a761af57120a0
yanelys-mena <yanelyshm@outlook.com>
2022-03-08 18:30:18 -0500

    review form modal in progress

--- react-app/src/components/PurchasesPage/Review.js
+++ react-app/src/components/PurchasesPage/Review.js
@@ -1,12 +1,16 @@
+import ReviewModal from "../ReviewModal";
+
 function Review({ reviews, purchase }) {
 
     // if the purchased product already exists in the review state, then edit the form and pre propulate information. 
     // otherwise, the form will be a new form and post a a new.
+    let message;
 
     return (
         <div>
-            {reviews[purchase.product_id] ? 'true' : 'false'}
-        </div>
+            {reviews[purchase.product_id] ? message = 'Edit Review' : 'Add Review'}
+            {/* <ReviewModal message={message} /> */}
+        </div >
     )
 };
 
---
+++ react-app/src/components/ReviewModal/index.js
@@ -0,0 +1,17 @@
+import './ReviewForm.css'
+import { useState } from 'react';
+import { Modal } from '../../context/Modal';
+// import ReviewForm from './ReviewForm';
+
+
+function ReviewModal({ message }) {
+    const [showModal, setShowModal] = useState(false);
+
+    return (
+        <>
+            {/* <button>{message}</button> */}
+        </>
+    )
+}
+
+export default ReviewModal;
