0c9aaf8ccfc49566c768e62d4256e4801a350389
AndrewPMurray <andrewpmurray1987@gmail.com>
2022-03-09 18:29:41 -0500

    added functionality to save cart state to local storage

--- react-app/src/components/Cart/index.js
+++ react-app/src/components/Cart/index.js
@@ -1,41 +1,51 @@
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { loadProducts } from '../../store/products';
 import CartItem from './CartItem';
 
 function Cart() {
-  const cart = useSelector(state => state.cart);
-  const products = useSelector(state => state.products);
+	const cart = useSelector((state) => state.cart);
+	const products = useSelector((state) => state.products);
+	const dispatch = useDispatch();
 
-  const cartItems = Object.values(cart).map(item => {
-    return { ...item, ...products[item.id] };
-  });
+	useEffect(() => {
+		window.localStorage.setItem('cart', JSON.stringify(cart));
+	}, [cart]);
 
-  if (!cartItems || !cartItems.length) return (
-    <div className="cart">
-      No items in the cart. Start selecting items to purchase.
-    </div>
-  );
+	useEffect(() => {
+		dispatch(loadProducts());
+	}, [dispatch]);
 
-  console.log("CART", cartItems);
+	const cartItems = Object.values(cart).map((item) => {
+		return { ...item, ...products[item.id] };
+	});
 
-  const onSubmit = (e) => {
-    e.preventDefault();
-    window.alert(
-      "Purchased the following:\n" +
-      `${cartItems.map(item => `${item.count} of ${item.title}`).join('\n')}`
-    );
-  }
+	if (!cartItems || !cartItems.length)
+		return <div className='cart'>No items in the cart. Start selecting items to purchase.</div>;
 
-  return (
-    <div className="cart">
-      <ul>
-        {cartItems.map(item => <CartItem key={item.id} item={item} />)}
-      </ul>
-      <hr />
-      <form onSubmit={onSubmit}>
-        <button type="submit">Purchase</button>
-      </form>
-    </div>
-  )
+	console.log('CART', cartItems);
+
+	const onSubmit = (e) => {
+		e.preventDefault();
+		window.alert(
+			'Purchased the following:\n' +
+				`${cartItems.map((item) => `${item.count} of ${item.title}`).join('\n')}`
+		);
+	};
+
+	return (
+		<div className='cart'>
+			<ul>
+				{cartItems.map((item) => (
+					<CartItem key={item.id} item={item} />
+				))}
+			</ul>
+			<hr />
+			<form onSubmit={onSubmit}>
+				<button type='submit'>Purchase</button>
+			</form>
+		</div>
+	);
 }
 
 export default Cart;
