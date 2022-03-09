054908a29d315077ac7e71cc23d96bbc827f1677
Whitney Minson <whitnessprints@gmail.com>
2022-03-09 12:38:55 -0800

    slight updates to review component

--- react-app/src/components/Reviews/index.js
+++ react-app/src/components/Reviews/index.js
@@ -1,49 +1,49 @@
-import { useState, useLayoutEffect } from 'react';
+import { useState, useEffect } from 'react';
 import './Reviews.css';
 import SingleReview from './SingleReview';
 
 function Reviews({ product, products }) {
-	const sellerProducts = Object.values(products).filter((p) => p?.user_id === product.user?.id);
+    const sellerProducts = Object.values(products).filter((p) => p?.user_id === product.user?.id);
+    let allReviews = [];
+    sellerProducts.forEach((p) => {
+        allReviews.push(...p.reviews);
+    });
 
-	const [noItemReviews, setNoItemReviews] = useState(false);
-	const [noReviews, setNoReviews] = useState(false);
-
-    const [showItemReviews, setShowItemReviews] = useState(false)
-    const [showSellerReviews, setSellerReviews] = useState(false)
+    const [showItemReviews, setShowItemReviews] = useState(!!product.reviews.length)
+    const [showSellerReviews, setSellerReviews] = useState(!!allReviews.length)
+    
+    console.log(showItemReviews, showSellerReviews)
 
 	// Yanelys' Avg Rating!
 	const reviews = Object.values(product?.reviews);
 	const ratings = [];
 	if (reviews) {
-		for (let i = 0; i < reviews.length; i++) {
-			ratings.push(reviews[i].rating);
+        for (let i = 0; i < reviews.length; i++) {
+            ratings.push(reviews[i].rating);
 		}
 	}
 	const averageRating = ratings.reduce((a, b) => a + b, 0) / reviews.length;
 	const stars = [];
 	for (let i = 0; i < averageRating; i++) {
-		stars.push(i);
+        stars.push(i);
 	}
-
-	let allReviews = [];
-
-	sellerProducts.forEach((p) => {
-		allReviews.push(...p.reviews);
-	});
-
-	useLayoutEffect(() => {
-		if (!product.reviews.length) setNoItemReviews(true);
-		if (!product.reviews.length && !allReviews) setNoReviews(true);
-	}, []);
+    
+    
 
 	let content;
 
-	if (!noItemReviews) {
+	if (product.reviews.length && allReviews.length) {
 		content = (
 			<div className='reviews-map-div'>
-				{product.reviews.map((review) => (
+				{showItemReviews ?
+                    product.reviews.map((review) => (
 					<SingleReview review={review} />
-				))}
+				    )) :
+                    allReviews.map((review) => (
+					<SingleReview review={review} />
+				    ))
+                    
+                }
 			</div>
 		);
 	}
@@ -68,15 +68,15 @@ function Reviews({ product, products }) {
 					</p>
 				</div>
 			</div>
-			{noReviews ? (
+			{(!product.reviews.length && !allReviews.length) ? 
 				<div>
 					<h3>No reviews yet for this seller</h3>
 					<p>Buy this item to leave the first review</p>
 				</div>
-			) : (
+			 : 
 				<div className='reviews-body'>
 					<div className='reviews-title-bar'>
-						{!noItemReviews && (
+						{!product.reviews.length && (
 							<div className='reviews-item-button'>
 								<p>Reviews for this item</p>
 								<p className='review-total'>{product?.reviews?.length}</p>
@@ -88,7 +88,7 @@ function Reviews({ product, products }) {
 						</div>
 					</div>
 				</div>
-			)}
+			}
 			<div className='reviews-body-div'>{content}</div>
 		</div>
 	);
