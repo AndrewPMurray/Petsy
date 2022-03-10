6e2a25a7deb0525756944f9d5c2d900f17ffb9f0
yanelys-mena <yanelyshm@outlook.com>
2022-03-10 15:10:34 -0500

    Fully Functional Drag and Drop

--- react-app/src/components/ReviewModal/UploadReviewImage.js
+++ react-app/src/components/ReviewModal/UploadReviewImage.js
@@ -5,41 +5,41 @@ import { FileUploader } from 'react-drag-drop-files'
 function UploadReviewImage({ review, setUrl }) {
     const [image, setImage] = useState(null);
     const fileTypes = ['JPG', 'PNG', 'HEIC', ' JPEG', 'jpg', 'jpeg']
-    // const updateImage = (e) => {
-    //     const file = e.target.files[0]
-    //     setImage(file)
-    //     // console.log(image, 'from UPLOAD FORM')
-    //     setUrl(file)
-    // };
 
     const setFile = (file) => {
-        console.log('FILE', file)
         setImage(file)
         setUrl(file)
     }
 
     return (
         <>
-            {/* <input type='file' accept='image/*' onChange={updateImage} /> */}
-            <FileUploader
-                handleChange={(file) => setFile(file)}
-                name='image'
-                types={fileTypes}>
-            </FileUploader>
-
-            <div id='picture-preview-container'>
-                <div className='picture-preview-node'>
-                    <i
-                        id='delete-picture'
-                        className='fas fa-times'
-                    ></i>
-                    {/* <img
-                        src={image ? URL.createObjectURL(image) : review?.url}
-                        style={{ maxHeight: '100px' }}
-                        alt='preview-upload'
-                    /> */}
-                </div>
+            {/* <div id="dragDropImageDiv"> */}
+            <div id="dragDropImageDiv">
+                <img
+                    src={image ? URL.createObjectURL(image) : review?.url}
+                    alt='preview-upload'
+                />
             </div>
+
+            <div id="dragDropArea">
+                <FileUploader
+                    id="fileUploader"
+                    handleChange={(file) => setFile(file)}
+                    name='image'
+                    types={fileTypes}
+                // label='Upload an Image or Drop here'
+                >
+                    <div id="dragDropDiv">Upload or Drag and Drop</div>
+                </FileUploader>
+            </div>
+
+            {/* </div> */}
+
+
+
+
+
+
         </>
     )
 
