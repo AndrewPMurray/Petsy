5be30f475fa695629c93c34f6e501b5f25018f88
AndrewPMurray <andrewpmurray1987@gmail.com>
2022-03-10 19:39:47 -0500

    fixed syntax error for review when image not included

--- app/api/reviews_routes.py
+++ app/api/reviews_routes.py
@@ -25,20 +25,27 @@ def create_review():
   form['csrf_token'].data = request.cookies['csrf_token']
   data = form.data
   image = form.image.data
-  image_url = None
   
-  if "image" in request.files:
-    image = request.files["image"]
-    if not allowed_file(image.filename):
+  if "image" not in request.files:
+    return {"errors": "image required"}, 400
+
+  image = request.files["image"]
+  # product_id = request.form['product_id']
+  
+  if not allowed_file(image.filename):
       return {"errors": "file type not permitted"}, 400
-    image.filename = get_unique_filename(image.filename)
-    upload = upload_file_to_s3(image)
-    if "url" not in upload:
+  
+  image.filename = get_unique_filename(image.filename)
+  
+  upload = upload_file_to_s3(image)
+  
+  if "url" not in upload:
       # if the dictionary doesn't have a url key
       # it means that there was an error when we tried to upload
       # so we send back that error message
       return upload, 400
-    image_url = upload["url"] 
+
+  image_url = upload["url"] 
 
   if form.validate_on_submit():
     new_review = Review(
