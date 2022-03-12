28c4119def6770b863b0014826ec3584c990bb25
AndrewPMurray <andrewpmurray1987@gmail.com>
2022-03-11 20:31:33 -0500

    implemented logic to redirect if landing on a pet-type, product-type, or product page that doesn't exist

--- react-app/src/components/ProductPage/index.js
+++ react-app/src/components/ProductPage/index.js
@@ -1,48 +1,46 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import { loadProducts } from '../../store/products';
 import ProductPhotos from './ProductPhotos';
 import ProductInfo from './ProductInfo';
-import Reviews from '../Reviews/index'
-import './ProductPage.css'
+import Reviews from '../Reviews/index';
+import './ProductPage.css';
 
 function ProductPage() {
-  const dispatch = useDispatch();
-  const { productId } = useParams();
-  const products = useSelector(state => state?.products)
+	const dispatch = useDispatch();
+	const { productId } = useParams();
+	const products = useSelector((state) => state?.products);
+	const history = useHistory();
 
-  let product;
-  let firstImage;
+	let product;
+	let firstImage;
 
-  if (products) {
-    product = products[productId]
-    firstImage = product?.images[0].id
-  }
+	if (products) {
+		product = products[productId];
+		if (!product) history.push('/not-found');
+		firstImage = product?.images[0].id;
+	}
 
-  useEffect(() => {
-    dispatch(loadProducts())
-  }, [dispatch])
+	useEffect(() => {
+		dispatch(loadProducts());
+	}, [dispatch]);
 
-  return (
-    <>
-      {
-        product && (
-          <div className='product-div'>
-            <div className='product-left-col'>
-              <ProductPhotos firstImage={firstImage} product={product} />
-              <Reviews product={product} products={products} />
-            </div>
-            <div className='product-right-col'>
-              <ProductInfo product={product} />
-            </div>
-          </div>
-        )
-      }
-    </>
-  );
+	return (
+		<>
+			{product && (
+				<div className='product-div'>
+					<div className='product-left-col'>
+						<ProductPhotos firstImage={firstImage} product={product} />
+						<Reviews product={product} products={products} />
+					</div>
+					<div className='product-right-col'>
+						<ProductInfo product={product} />
+					</div>
+				</div>
+			)}
+		</>
+	);
 }
-
-
 
 export default ProductPage;
--- react-app/src/components/ProductTypePage/index.js
+++ react-app/src/components/ProductTypePage/index.js
@@ -1,6 +1,6 @@
 import './ProductTypePage.css';
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import { loadProductTypesByPet } from '../../store/productTypes';
 import { useEffect } from 'react';
 import DetailedProductGrid from '../DetailedProductGrid';
@@ -9,21 +9,26 @@ export default function ProductTypePage() {
 	const dispatch = useDispatch();
 	const { petTypeId, productTypeId } = useParams();
 	const products = useSelector((state) => Object.values(state?.productTypes));
+	const history = useHistory();
 
 	useEffect(() => {
-		dispatch(loadProductTypesByPet(petTypeId, productTypeId));
+		dispatch(loadProductTypesByPet(petTypeId, productTypeId)).then((res) =>
+			res.length === 0 ? history.push('/not-found') : res
+		);
 	}, [dispatch, petTypeId, productTypeId]);
 
 	return (
 		<>
-			<div id="product-type-header">
+			<div id='product-type-header'>
 				<div id='main-titles'>
-					<div id="pet-type-title">{products[0].pet_type?.title} {products[0].product_type?.title}</div>
+					<div id='pet-type-title'>
+						{products[0].pet_type?.title} {products[0].product_type?.title}
+					</div>
 				</div>
 			</div>
-			<div id="product-type-page">
+			<div id='product-type-page'>
 				<DetailedProductGrid products={products} />
 			</div>
 		</>
-	)
+	);
 }
