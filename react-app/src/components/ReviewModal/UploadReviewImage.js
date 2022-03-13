92afc8dfcbffbebdca5f5c3d1a72ec08ca7344fc
AndrewPMurray <andrewpmurray1987@gmail.com>
2022-03-13 15:26:53 -0400

    review image changes

--- react-app/src/components/ReviewModal/UploadReviewImage.js
+++ react-app/src/components/ReviewModal/UploadReviewImage.js
@@ -14,14 +14,28 @@ function UploadReviewImage({ review, setUrl, url, setImageToDelete }) {
 	return (
 		<>
 			<div id='dragDropImageDiv'>
-				<img
-					src={
-						image
-							? URL.createObjectURL(image)
-							: 'https://www.gaithersburgdental.com/wp-content/uploads/2016/10/orionthemes-placeholder-image.png'
-					}
-					alt='preview-upload'
-				/>
+				{!review && (
+					<img
+						src={
+							image
+								? URL.createObjectURL(image)
+								: 'https://www.gaithersburgdental.com/wp-content/uploads/2016/10/orionthemes-placeholder-image.png'
+						}
+						alt='preview-upload'
+					/>
+				)}
+				{review && (
+					<img
+						src={
+							review?.url.length > 0
+								? image
+									? URL.createObjectURL(image)
+									: review.url
+								: 'https://www.gaithersburgdental.com/wp-content/uploads/2016/10/orionthemes-placeholder-image.png'
+						}
+						alt='preview-upload'
+					/>
+				)}
 			</div>
 
 			<div id='dragDropArea'>
