f5b8a7cac6c27fc641a4c8cc02d6e8558ec51a54
Whitney Minson <whitnessprints@gmail.com>
2022-12-23 15:30:55 -0800

    alter table with schema name fixed

--- migrations/versions/38064ebd07e2_.py
+++ migrations/versions/38064ebd07e2_.py
@@ -92,7 +92,13 @@ def upgrade():
     sa.PrimaryKeyConstraint('id')
     )
     if environment == "production":
-        op.execute(f"ALTER TABLE <table_name> SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE reviews SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE purchases SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE images SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE products SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE product_types SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE pet_types SET SCHEMA {SCHEMA};")
     # ### end Alembic commands ###
 
 
